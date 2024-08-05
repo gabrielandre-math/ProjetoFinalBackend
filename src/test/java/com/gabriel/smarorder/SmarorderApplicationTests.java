df11d2113970d2f480017d740491c2c3242f3bb9
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-05 03:05:18 -0300

    project started

---
+++ src/test/java/com/gabriel/smarorder/SmarorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.smarorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
