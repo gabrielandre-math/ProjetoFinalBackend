c56ea46fa9966f6ba38dd48af247386de0ad927d
gabrielandré <gabrieldevpyc@gmail.com>
2024-08-06 02:54:08 -0300

    feat: add small security feature to initialize our project

--- src/main/java/com/gabriel/smarorder/SecurityConfig.java
+++ src/main/java/com/gabriel/smarorder/SecurityConfig.java
@@ -1,2 +1,31 @@
-package com.gabriel.smarorder;public class SecurityConfig {
+package com.gabriel.smarorder;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**") // Ignorar proteção CSRF para o console H2
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                );
+        return http.build();
+    }
 }
